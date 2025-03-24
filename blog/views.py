a403fb0ac44abcddcd1d79fe52e9e49c15555634
nurbekomar <omarnurbek13@gmail.com>
2025-03-24 11:07:21 +0500

    add handler404

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseNotFound
 
 
 # Create your views here.
 def home(request):
     return HttpResponse("Hello Django!")
+
+
+def page_not_found(request, exception):
+    return HttpResponseNotFound("<h1>Бұл бет табылмады!</h1>")
